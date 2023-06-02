c25b6a6fd2fa5f5dea71f2e1d87937a6cd512dbc
brickwall <sk292207@gmail.com>
2023-06-03 00:23:58 +0530

    Add post scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :posts
   resources :friendships, except: [:destroy]
   delete '/friendships', to: 'friendships#destroy'
   post '/send_request', to: 'friendships#request_create'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :post do
+    
+  end
+
   factory :user do
     sequence(:username) { |n| "user_#{n}" }
     sequence(:email) { |n| "user_#{n}@example.org" }
