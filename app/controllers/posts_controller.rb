f8269cd7e2043ad814f943b1dc46444fd3dbd119
brickwall <sk292207@gmail.com>
2023-06-03 16:56:45 +0530

    Add feature so user can update and destroy their post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-  before_action :set_username, only: %i[ create ]
+  before_action :set_username, only: %i[ create update destroy ]
   before_action :authenticate_user!
 
   # GET /posts or /posts.json
@@ -40,8 +40,8 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+      if (current_user.username == @user.username) && @post.update(post_params)
+        format.html { redirect_to post_url(current_user.username,@post), notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
-    @post.destroy
+    (current_user.username == @user.username) && @post.destroy
 
     respond_to do |format|
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
