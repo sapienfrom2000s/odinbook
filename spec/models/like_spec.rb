9ea43e602decc87a812a1cac548f476003c907d3
brickwall <sk292207@gmail.com>
2023-06-06 15:29:31 +0530

    Add tests for Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+    user = create(:user)
+    post = Post.new(title:'this is more than 10 chars', body:'this is....................................
+      .................more than 30 chars')
+    post.creator = user
+    post.save
+  end
+  context 'when a post is liked' do
+    it 'creates association bw user and the post' do
+      expect{Like.create(user_id:User.first.id, post_id: Post.first.id)}.to change{Like.count}
+    end
+  end
+
+  context 'when a like already exists' do
+    it 'rolls back the transaction' do
+      Like.create(user_id:User.first.id, post_id: Post.first.id)
+      expect{Like.create(user_id:User.first.id, post_id: Post.first.id)}.to_not change{Like.count}      
+    end
+  end
+  
+  context 'when a post is unliked' do
+    it 'destroys the association between user and the post' do 
+      Like.create(user_id:User.first.id, post_id: Post.first.id)
+      expect{Like.first.destroy}.to change{Like.count}.by(-1)            
+    end
+  end
 end
