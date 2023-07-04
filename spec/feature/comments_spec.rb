a95195bbf3e090db7c9821928bd2a8fe50b8dcdf
brickwall <sk292207@gmail.com>
2023-07-04 18:16:53 +0530

    Add system tests for comment and reply feature

--- spec/feature/comments_spec.rb
+++ spec/feature/comments_spec.rb
@@ -13,6 +13,8 @@ feature 'Make comment on post', type: :system do
     fill_in 'Password', with: 'password'
     click_button 'Log in'
 
+    sleep 2
+
     visit "/user_1/posts/#{User.first.posts.first.id}"
 
     comment_box = page.find('#comment_body').fill_in with: 'some random comment'
@@ -22,10 +24,13 @@ feature 'Make comment on post', type: :system do
   end
 
   scenario 'make a reply to root comment' do
+
+    visit '/users/sign_in'
     fill_in 'Login', with: 'user_2'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
 
+    sleep 2
     visit "/user_2/posts/#{User.first.posts.first.id}"
 
     comment_box = page.find('#comment_body').fill_in with: 'some random comment'
@@ -42,10 +47,12 @@ feature 'Make comment on post', type: :system do
   end
 
   scenario 'make two replies to root comment' do
+    visit '/users/sign_in'
     fill_in 'Login', with: 'user_3'
     fill_in 'Password', with: 'password'
     click_button 'Log in'
 
+    sleep 3
     visit "/user_3/posts/#{User.first.posts.first.id}"
 
     comment_box = page.find('#comment_body').fill_in with: 'some random comment'
@@ -59,12 +66,45 @@ feature 'Make comment on post', type: :system do
 
     expect(page).to have_content('Comment was successfully made.')
 
-    page.find_all('.reply-link').first.click
+    page.find_all('.reply-link a').first.click
     
     reply_box2 = page.find_all('#comment_body').first
-    reply_box2.fill_in with: 'another child of some random comment'
-    page.find 'Reply'
 
-    # page.assert_selector('.nesting', count: 1)
+    reply_box2.fill_in with: 'another child of some random comment'
+
+    click_button 'Reply'
+
+    page.assert_selector('.nesting', count: 1)
+  end
+
+  scenario 'make two level replies(root --> reply --> reply) to the root comment' do
+    visit '/users/sign_in'
+    fill_in 'Login', with: 'user_4'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+    sleep 3
+    visit "/user_4/posts/#{User.first.posts.first.id}"
+
+    comment_box = page.find('#comment_body').fill_in with: 'some random comment'
+    
+    click_on 'Comment'
+    click_link 'Reply'
+
+    reply_box = page.find_all('#comment_body').first
+    reply_box.fill_in with: 'child of some random comment'
+    click_button 'Reply'
+
+    expect(page).to have_content('Comment was successfully made.')
+
+    page.find_all('.reply-link a').last.click
+    
+    reply_box2 = page.find_all('#comment_body').first
+
+    reply_box2.fill_in with: 'grand child of some random comment'
+
+    click_button 'Reply'
+
+    page.assert_all_of_selectors(:css, '.nesting .nesting')
   end
 end
